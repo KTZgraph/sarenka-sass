441cfaac148033b7cd376ed4d425b5b84d280343
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2021-12-12 15:35:11 +0100

    4 koniec podzial sass na pliki

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const {src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-    return src('index.scss') // wejsciowy plik
+    return src('*.scss') // wejsciowy plik
         .pipe(sass())
         .pipe(dest('css')) // wyjsociwy foldrer
 }
 
 function watchTask(){
-    watch(['index.scss'], buildStyles) // nasluchiwanie zmain - odpallenie co modyfikacja
+    watch(['*.scss'], buildStyles) // nasluchiwanie zmain - odpallenie co modyfikacja
 }
 
 exports.default = series(buildStyles, watchTask) // kompilacja w kolejnosci
