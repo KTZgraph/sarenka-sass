b152bd318d6c493895817b831e5c675c2443c5f0
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2021-12-12 15:44:01 +0100

    5 koniec poorzadek w folderze sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const {src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-    return src('*.scss') // wejsciowy plik
+    return src('sarenka-sass/**/*.scss') // wejsciowy plik
         .pipe(sass())
         .pipe(dest('css')) // wyjsociwy foldrer
 }
 
 function watchTask(){
-    watch(['*.scss'], buildStyles) // nasluchiwanie zmain - odpallenie co modyfikacja
+    watch(['sarenka-sass/**/*.scss'], buildStyles) // nasluchiwanie zmain - odpallenie co modyfikacja
 }
 
 exports.default = series(buildStyles, watchTask) // kompilacja w kolejnosci
