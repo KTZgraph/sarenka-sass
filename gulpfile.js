591f11b857f8b692ed229410d44818d0131ca1d5
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2021-12-14 21:42:52 +0100

    21 koniec !default nadpisywanie wartosci zmiennych

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ const sass = require('gulp-sass')(require('sass'));
 const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
-    return src('sarenka-sass/**/*.scss') // wejsciowy plik
+    // return src('sarenka-sass/**/*.scss') // wejsciowy plik
+    return src('sass/**/*.scss') // zmiana nasluchiwania gdy nadpisuje biblioteke
         .pipe(sass())
         .pipe(purgecss({content: ['*.html']})) // zmniejszony css - ma tylko klasy ktore sa uzywane
         .pipe(dest('css')) // wyjsociwy foldrer
@@ -11,7 +12,8 @@ function buildStyles() {
 
 function watchTask(){
     //rebuild gdy zmieni sie jaskiś .scss albo jakiś html 
-    watch(['sarenka-sass/**/*.scss', '*.html'], buildStyles) // nasluchiwanie zmain - odpallenie co modyfikacja
+    // watch(['sarenka-sass/**/*.scss', '*.html'], buildStyles) // nasluchiwanie zmain - odpallenie co modyfikacja
+    watch(['sass/**/*.scss', '*.html'], buildStyles) // zmiana nasluchiwania gdy nadpisuje biblioteke
 }
 
 exports.default = series(buildStyles, watchTask) // kompilacja w kolejnosci
